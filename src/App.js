c84f7309ec8d1c401244ea66d2eae093a966a1e3
Moniratna <moniratna.munshi123@gmail.com>
2021-07-06 09:12:03 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tesseract from "tesseract.js";
+
+
+
+const loadData = async () => {
+  const result = await Tesseract.recognize(
+    "https://i.iib.co/VHb6y10/Artboard-1.png",
+    "eng",
+    {logger: m => console.log(m)}
+    );
+    console.log(result);
+}
+
+loadData();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://i.ibb.co/LpxDvR1/logo.png" className="logo" />
+      <div className="container">
+        <div className="image-wrapper">
+          <form>
+            <input type="file" className="custom-file-input" name="image" />
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
