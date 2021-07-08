aecbc2ae67721bb24c10c80acb227b0cda1d7ca6
moniratna <moniratna.munshi123@gmail.com>
2021-07-08 20:19:24 +0530

    navbar changed

--- src/components/NewNav.js
+++ src/components/NewNav.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NewNav() {
     return (
         <nav class="navbar navbar-light bg-light justify-content-between">
-        <img src="https://i.ibb.co/LpxDvR1/logo.png" className="logo" alt="" />
+        <a class="navbar-brand"><img src="https://img.icons8.com/ios/50/000000/apple-camera.png"/>Image To Text</a>
         <form class="form-inline">
           {/* <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> */}
           <button class="btn btn-outline-success my-2 my-sm-0" onClick={()=>{window.location.reload()}} type="submit">Reset</button>
@@ -12,3 +12,4 @@ export default function NewNav() {
       
     )
 }
+{/* <img src="https://i.ibb.co/LpxDvR1/logo.png" className="logo" alt="" /> */}
